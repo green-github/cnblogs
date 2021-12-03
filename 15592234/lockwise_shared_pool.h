318c24f855303ab9bfdc89aaa3bed87e7abc7bed
green-github <15040298@qq.com>
2021-12-03 21:55:51 +0800

    Refactored.

---
+++ 15592234/lockwise_shared_pool.h
@@ -0,0 +1,134 @@
+/*
+ * lockwise_shared_pool.h
+ *
+ * A simple thread pool using a shared task queue among worker threads,
+ * accepting callables as tasks.
+ *
+ */
+
+#ifndef LOCKWISE_SHARED_POOL_H
+#define LOCKWISE_SHARED_POOL_H
+
+#include <cstdio>
+
+#include <atomic>
+#include <future>
+#include <thread>
+#include <type_traits>
+#include <utility>
+
+#include "lockwise_queue.h"
+
+
+using std::atomic;
+using std::future;
+using std::memory_order_acquire;
+using std::memory_order_release;
+using std::packaged_task;
+using std::thread;
+
+
+class Thread_Pool {
+
+  private:
+
+    struct Task_Wrapper {
+
+        struct Task_Base {
+            virtual ~Task_Base() {}
+            virtual void call() = 0;
+        };
+        template<class T>
+        struct Task : Task_Base {
+            T _t_;
+            Task(T&& t) : _t_(std::move(t)) {}
+            void call() { _t_(); }
+        };
+
+        Task_Base* _ptr_;
+
+        Task_Wrapper() : _ptr_(nullptr) {};
+        // support move
+        Task_Wrapper(Task_Wrapper&& other) {
+            _ptr_ = other._ptr_;
+            other._ptr_ = nullptr;
+        }
+        Task_Wrapper& operator=(Task_Wrapper&& other) {
+            _ptr_ = other._ptr_;
+            other._ptr_ = nullptr;
+            return *this;
+        }
+        // no copy
+        Task_Wrapper(Task_Wrapper&) = delete;
+        Task_Wrapper& operator=(Task_Wrapper&) = delete;
+        ~Task_Wrapper() {
+            if (_ptr_) delete _ptr_;
+        }
+        template<class T>
+        Task_Wrapper(T&& t) : _ptr_(new Task<T>(std::move(t))) {}
+
+        void operator()() const {
+            _ptr_->call();
+        }
+
+    };
+
+    atomic<bool> _done_;
+    Lockwise_Queue<Task_Wrapper> _queue_;
+    unsigned _workersize_;
+    thread* _workers_;
+
+    void work() {
+        Task_Wrapper task;
+        while (!_done_.load(memory_order_acquire)) {
+            if (_queue_.pop(task))
+                task();
+            else
+                std::this_thread::yield();
+        }
+    }
+
+    void stop() {
+        size_t remaining = _queue_.size();
+        while (!_queue_.empty())
+            std::this_thread::yield();
+        std::fprintf(stderr, "\n%zu tasks remain before destructing pool.\n", remaining);
+        _done_.store(true, memory_order_release);
+        for (unsigned i = 0; i < _workersize_; ++i) {
+            if (_workers_[i].joinable())
+                _workers_[i].join();
+        }
+        delete[] _workers_;
+    }
+
+  public:
+    Thread_Pool() : _done_(false) {
+        try {
+            _workersize_ = thread::hardware_concurrency();
+            _workers_ = new thread[_workersize_];
+            for (unsigned i = 0; i < _workersize_; ++i) {
+                _workers_[i] = thread(&Thread_Pool::work, this);
+            }
+        } catch (...) {
+            stop();
+            throw;
+        }
+    }
+    ~Thread_Pool() {
+        stop();
+    }
+
+    template<class Callable>
+    future<typename std::result_of<Callable()>::type> submit(Callable c) {
+        typedef typename std::result_of<Callable()>::type R;
+        packaged_task<R()> task(c);
+        future<R> r = task.get_future();
+        _queue_.push(std::move(task));
+        return r;
+    }
+
+};
+
+
+#endif
+
---
+++ 15592234/test.cpp
@@ -0,0 +1,167 @@
+/*
+ * test.cpp
+ *
+ * Testing Lockwise_Shared_Pool.
+ *
+ */
+
+#include <cstdio>
+
+#include <chrono>
+#include <functional>
+#include <future>
+#include <thread>
+
+#include "archery.h"
+#include "lockwise_shared_pool.h"
+
+
+using std::atomic;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+using std::chrono::minutes;
+using std::chrono::steady_clock;
+using std::chrono::time_point;
+using std::future;
+using std::thread;
+
+
+int main() {
+    minutes PERIOD(1);
+    size_t counter[11];
+    time_point<steady_clock> start;
+    atomic<bool> go(false);
+
+    {
+        Thread_Pool pool;
+
+        thread t1([PERIOD, &counter, &start, &go, &pool] {        // test free function of void() 
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            void (*task)() = shoot;
+            for (counter[1] = 0; steady_clock::now() - start <= PERIOD; ++counter[1]) {
+                pool.submit(task);
+                //pool.submit(std::bind<void(*)()>(shoot));
+                std::this_thread::yield();
+            }
+        });
+
+        thread t2([PERIOD, &counter, &start, &go, &pool] {        // test free function of bool(size_t)
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            bool (*task)(size_t) = shoot;
+            for (counter[2] = 0; steady_clock::now() - start <= PERIOD; ++counter[2]) {
+                future<bool> r = pool.submit(std::bind(task, counter[2]));
+                //future<bool> r = pool.submit(std::bind<bool(*)(size_t)>(shoot, counter[2]));
+                std::this_thread::yield();
+            }
+        });
+
+        thread t3([PERIOD, &counter, &start, &go, &pool] {        // test lambda of void()
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            for (counter[3] = 0; steady_clock::now() - start <= PERIOD; ++counter[3]) {
+                pool.submit(shootAnarrow);
+                std::this_thread::yield();
+            }
+        });
+
+        thread t4([PERIOD, &counter, &start, &go, &pool] {        // test lambda of bool(size_t)
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            for (counter[4] = 0; steady_clock::now() - start <= PERIOD; ++counter[4]) {
+                future<bool> r = pool.submit(std::bind(shootNarrows, counter[4]));
+                std::this_thread::yield();
+            }
+        });
+
+        thread t5([PERIOD, &counter, &start, &go, &pool] {        // test functor of void()
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            Archer hoyt;
+            for (counter[5] = 0; steady_clock::now() - start <= PERIOD; ++counter[5]) {
+                pool.submit(hoyt);
+                std::this_thread::yield();
+            }
+        });
+
+        thread t6([PERIOD, &counter, &start, &go, &pool] {        // test functor of bool(size_t)
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            Archer hoyt;
+            for (counter[6] = 0; steady_clock::now() - start <= PERIOD; ++counter[6]) {
+                future<bool> r = pool.submit(std::bind(hoyt, counter[6]));
+                std::this_thread::yield();
+            }
+        });
+
+        thread t7([PERIOD, &counter, &start, &go, &pool] {        // test member function of void()
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            Archer hoyt;
+            for (counter[7] = 0; steady_clock::now() - start <= PERIOD; ++counter[7]) {
+                pool.submit(std::bind<void(Archer::*)()>(&Archer::shoot, &hoyt));
+                //pool.submit(std::bind(static_cast<void(Archer::*)()>(&Archer::shoot), &hoyt));
+                std::this_thread::yield();
+            }
+        });
+
+        thread t8([PERIOD, &counter, &start, &go, &pool] {        // test member function of bool(size_t)
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            Archer hoyt;
+            for (counter[8] = 0; steady_clock::now() - start <= PERIOD; ++counter[8]) {
+                future<bool> r = pool.submit(std::bind<bool(Archer::*)(size_t)>(&Archer::shoot, &hoyt, counter[8]));
+                //future<bool> r = pool.submit(std::bind(static_cast<bool(Archer::*)(size_t)>(&Archer::shoot), &hoyt, counter[8]));
+                std::this_thread::yield();
+            }
+        });
+
+        thread t9([PERIOD, &counter, &start, &go, &pool] {        // test std::function<> of void()
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            std::function<void()> task = static_cast<void(*)()>(shoot);
+            for (counter[9] = 0; steady_clock::now() - start <= PERIOD; ++counter[9]) {
+                pool.submit(task);
+                std::this_thread::yield();
+            }
+        });
+
+        thread t10([PERIOD, &counter, &start, &go, &pool] {        // test std::function<> of bool(size_t)
+            while (!go.load(memory_order_acquire))
+                std::this_thread::yield();
+            std::function<bool(size_t)> task = static_cast<bool(*)(size_t)>(shoot);
+            for (counter[10] = 0; steady_clock::now() - start <= PERIOD; ++counter[10]) {
+                future<bool> r = pool.submit(std::bind(task, counter[10]));
+                std::this_thread::yield();
+            }
+        });
+
+        std::fprintf(stderr, "\nReady...Go\n\nWait a moment...\n");
+        std::this_thread::sleep_for(milliseconds(1000));
+        start = steady_clock::now();
+        go.store(true, memory_order_release);
+
+        t10.join();
+        t9.join();
+        t8.join();
+        t7.join();
+        t6.join();
+        t5.join();
+        t4.join();
+        t3.join();
+        t2.join();
+        t1.join();
+
+    }
+
+    for (unsigned i = 1; i < 11; ++i)
+        counter[0] += counter[i];
+    std::fprintf(stderr, "\n%zu tasks submitted in total.\n", counter[0]);
+    time_point<steady_clock> end = steady_clock::now();
+    std::fprintf(stderr, "\nTook %.3f seconds.\n", duration<double>(end - start).count());
+
+    std::fprintf(stderr, "\nBye...\n");
+    return 0;
+}
+
